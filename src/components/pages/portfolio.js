d06dcd62d02be29e70d2dad02f95024d120e4947
Undiess <nguyen.andy67@gmail.com>
2020-07-29 06:18:52 +0930

    added existing portoflio to react

---
+++ src/components/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Portfolio(){
+     return( 
+         <div> 
+             <h> 
+                 this is a portfolio page
+             </h>
+         </div>
+     )
+    }
+
+export default Portfolio
